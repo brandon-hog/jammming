6a915724c8ad4ea4a35d6d5bf037c9331c6505fe
brandon-hog <brandon.m.hoggatt@gmail.com>
2024-10-18 18:06:44 -0500

    committing before downgrading to see if anything breaks

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,14 @@
 import searchbar from './SearchBar.module.css';
 
 function SearchBar(props) {
+
+    // implement searching with get requests when user clicks button 
+
+
+
+
+
+
     return (
         <form >
             <input className={searchbar.searchbar} value={props.userSearch} onChange={e => props.setSearch(e.value)}></input>
